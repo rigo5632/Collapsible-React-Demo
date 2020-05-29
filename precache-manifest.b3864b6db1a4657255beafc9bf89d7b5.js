f2012a9b5560995625324e3ed6dd4f76d4c519ba
rigo5632 <rmquiroz5@gmail.com>
2020-05-29 08:51:49 -0600

    Updates

---
+++ precache-manifest.b3864b6db1a4657255beafc9bf89d7b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fd411e7ef85fb183a4761e3f2cb1f7",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "e2c491c35526503a177f",
+    "url": "/tabs/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "67404c9d1c2f92de501c",
+    "url": "/tabs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2c491c35526503a177f",
+    "url": "/tabs/static/js/2.ec3bbaf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabs/static/js/2.ec3bbaf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67404c9d1c2f92de501c",
+    "url": "/tabs/static/js/main.bdb493f2.chunk.js"
+  },
+  {
+    "revision": "799ebe26c0ee0db95f4d",
+    "url": "/tabs/static/js/runtime-main.4de0c85e.js"
+  }
+]);
